18cf5c23db56c0e0447919c66d86bea32efbeaca
Luca <lfcaruccio@gmail.com>
2025-03-19 20:42:07 -0700

    React Intro

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+import Todo from './Components/Todo.jsx';
+import Title from './Components/Title.jsx';
+import Modal from './Components/Modal.jsx';
+
+function App() {
+  return (
+    <div>
+      <Title />
+      <div className='todo__wrapper'>
+      
+      <Todo />
+      <Todo />
+      <Todo />
+      </div>
+      <Modal />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Todo.jsx
@@ -0,0 +1,12 @@
+import './Todo.css'
+
+function Todo() {
+    return (
+        <div className='todo'>
+        <h2>Finish FES</h2>
+        <button>DELETE</button>
+      </div>
+    )
+}
+
+export default Todo
